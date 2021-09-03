611b185ca1fcd97c32959ffdcb58cc11d0dfe128
FCPhenrique <henriquefcpereira@gmail.com>
2021-09-02 21:37:41 -0300

    teste2

--- testenode.js
+++ testenode.js
@@ -1,5 +1,21 @@
-var http = require("http");
+const express = require("express");
+const app = express();
 
-http.createServer(function(a,b){
-    b.end("aaaaaa");
-}).listen(8080);
+app.get("/", function (req, res) {
+    res.send("s")
+})
+app.get("/teste/nome", function (req, res) {
+    var canal = req.query["a"]
+    res.send(canal)
+
+
+})
+
+
+app.listen(8080, function (erro) {
+    if (erro) {
+        console.log("Erro detectado")
+    } else {
+        console.log("tudo certo")
+    }
+})
