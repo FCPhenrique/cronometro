49e6f6880f2d4139a64101046cf7eff5b2f8a3c2
FCPhenrique <henriquefcpereira@gmail.com>
2021-09-02 21:41:34 -0300

    aaaaa

---
+++ testenode.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+
+app.get("/", function (req, res) {
+    res.send("s")
+})
+app.get("/teste/nome", function (req, res) {
+    var canal = req.query["a"]
+    res.send(canal+"asdasdasdasd")
+
+
+
+})
+
+
+app.listen(8080, function (erro) {
+    if (erro) {
+        console.log("Erro detectado")
+    } else {
+        console.log("tudo certo")
+    }
+})
